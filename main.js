4ff91abd8cb5e8ccd63f29993340afc0cca4eb71
jmk2142 <jmk2142@tc.columbia.edu>
2016-12-14 19:14:13 -0500

    Edited your code

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 
-var el = document.getElementById("situation1").addEventListener("click", function() {
-    answer = prompt("Please enter your answer");
-	});
+// var el = document.getElementById("situation1").addEventListener("click", function() {
+//   answer = prompt("Please enter your answer");
+// });
 
 
   //});
 //get the modal
 var modal=document.getElementById("assessmentModal");
 //get the button that opens the modal
-var btn=document.getElementById("modalBtn").addEventlistener("click",
-function(event){
+var btn = document.getElementById("modalBtn");
+
+var q1Value = 0;
+var q2Value = 0;
+var qTotal;
+
+btn.addEventListener("click", function(event){
+	// Code to select the CHECKED one.
+	var q1Value = Number(document.querySelector('[name="q1"]:checked').value);
+	alert(q1Value);
   $("#assessmentModal").modal("show");
+	qTotal = q1Value + q2Value;
+
+	document.getElementById('good').innerHTML = qTotal + " This is your score";
+
+	if (qTotal <= 2) {
+		// document.getElementById('...').classList.add('doNotShow');
+	} else {
+		document.getElementById('great').classList.remove('doNotShow');
+	}
 });
 
 //var btn=document.getElementById("modalBtn");
